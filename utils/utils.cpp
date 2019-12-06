828b9a61192ae79d29e9d554079450f2936dce9f
Robert Gambee <robertgambee@gmail.com>
2019-12-05 20:59:56 -0500

    Use enum to represent opcodes

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -20,6 +20,21 @@ std::ifstream open_input_file(int argc, char **argv) {
 }
 
 
+Opcode int_to_opcode(int integer) {
+    switch(integer) {
+        case 1:
+            return Opcode::ADD;
+        case 2:
+            return Opcode::MULTIPLY;
+        case 99:
+            return Opcode::END;
+        default:
+            std::cerr << "Unknown opcode: " << integer << std::endl;
+            exit(3);
+    }
+}
+
+
 std::vector<unsigned int> load_intcode_program(std::istream &input_stream) {
     std::vector<unsigned int> numbers;
     std::string num_str;
@@ -32,32 +47,42 @@ std::vector<unsigned int> load_intcode_program(std::istream &input_stream) {
 
 unsigned int run_intcode_program(std::vector<unsigned int> numbers) {
     for (std::vector<int>::size_type i = 0; i < numbers.size() - 4; i += 4) {
-        auto opcode = numbers[i];
-        if (opcode == 99) {
-            // Done
-            break;
-        } else if (opcode != 1 && opcode != 2 ) {
-            std::cerr << "Unknown opcode: " << opcode << std::endl;
-            exit(3);
+        auto opcode = int_to_opcode(numbers[i]);
+        switch (opcode) {
+            case Opcode::END:
+                i = numbers.size();
+                break;
+            case Opcode::ADD:
+            case Opcode::MULTIPLY:
+            {
+                // 3 operands
+                auto input_index_a = numbers[i+1];
+                auto input_index_b = numbers[i+2];
+                auto output_index = numbers[i+3];
+                if (numbers.size() < input_index_a || numbers.size() < input_index_b
+                    || numbers.size() < output_index) {
+                    std::cerr << "Index out of range" << std::endl;
+                    exit(4);
+                }
+                int result = -1;
+                switch (opcode) {
+                    case Opcode::ADD:
+                        result = numbers[input_index_a] + numbers[input_index_b];
+                        break;
+                    case Opcode::MULTIPLY:
+                        result = numbers[input_index_a] * numbers[input_index_b];
+                        break;
+                    default:
+                        std::cerr << "Unexpected opcode: " << int(opcode) << std::endl;
+                        exit(3);
+                }
+                numbers[output_index] = result;
+                break;
+            }
+            default:
+                std::cerr << "Unexpected opcode: " << int(opcode) << std::endl;
+                exit(3);
         }
-        auto input_index_a = numbers[i+1];
-        auto input_index_b = numbers[i+2];
-        auto output_index = numbers[i+3];
-        if (numbers.size() < input_index_a || numbers.size() < input_index_b
-            || numbers.size() < output_index) {
-            std::cerr << "Index out of range" << std::endl;
-            exit(4);
-        }
-
-        int result = -1;
-        if (opcode == 1) {
-            // Addition
-            result = numbers[input_index_a] + numbers[input_index_b];
-        } else if (opcode == 2) {
-            // Multiplication
-            result = numbers[input_index_a] * numbers[input_index_b];
-        }
-        numbers[output_index] = result;
     }
     return numbers[0];
 }
--- utils/utils.h
+++ utils/utils.h
@@ -4,6 +4,15 @@
 
 std::ifstream open_input_file(int argc, char **argv);
 
+
+enum class Opcode {
+    ADD = 1,
+    MULTIPLY = 2,
+    END = 99
+};
+
+Opcode int_to_opcode(int integer);
+
 std::vector<unsigned int> load_intcode_program(std::istream &input_stream);
 
 unsigned int run_intcode_program(std::vector<unsigned int> numbers);
